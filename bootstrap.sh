21d46c800a3fc513e20dc070b9673b7db238d222
Kelvin Keegan <kelvinkeegen17@gmail.com>
2024-04-14 01:17:38 +0100

    point to my repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -133,7 +133,7 @@ if [ -d "$HOME/ansible-easy-vpn" ]; then
   git pull
   popd
 else
-  git clone https://github.com/notthebee/ansible-easy-vpn $HOME/ansible-easy-vpn
+  git clone https://github.com/kelvin-keegen/ansible-easy-vpn.git $HOME/ansible-easy-vpn
 fi
 
 # Set up a Python venv
